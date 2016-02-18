d50cb6a050380a3f2d489712f013b4d50b7e7c3f
Vlad Petrenko <quaternion94@hotmail.com>
2016-02-18 14:31:52 +0200

    repeating bank request

--- BankingAppNew.Core/Services/Impl/BankService.cs
+++ BankingAppNew.Core/Services/Impl/BankService.cs
@@ -7,13 +7,14 @@ using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 
 namespace BankingAppNew.Core.Services.Impl
 {
   public class BankService : IBankService
   {
     private IUnitOfWorkFactory _factory;
-
+    private const int AttemptsCount = 10;
     public BankService(IUnitOfWorkFactory factory)
     {
       this._factory = factory;
@@ -25,123 +26,173 @@ namespace BankingAppNew.Core.Services.Impl
         return new BankRequestResult<Decimal>(BankRequestStatus.Done, accountUnitOfWork.BankAccountRepository.GetByID(userid).AccountBalance);
     }
 
-    public BankRequestResult Deposit(string userid, Decimal amount)
-    {
-      using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+      public BankRequestResult Deposit(string userid, Decimal amount)
       {
-        try
-        {
-          BankAccount account = accountUnitOfWork.BankAccountRepository.GetByID(userid);
-          account.AccountBalance += amount;
-          accountUnitOfWork.TransferRepository.Add(new Transfer()
+          bool done = false;
+          int iterations = 1;
+          while (iterations<AttemptsCount && !done)
           {
-            Amount = amount,
-            BankAccountId = account.Id,
-            Username = account.UserName
-          });
-          accountUnitOfWork.BankAccountRepository.Update(account);
-        }
-        catch (DbUpdateConcurrencyException ex)
-        {
-          return new BankRequestResult(BankRequestStatus.Error, null, "Concurency error");
-        }
-      }
-      return new BankRequestResult();
+              iterations++;
+              try
+              {
+                  using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+                  {
+                      BankAccount account = accountUnitOfWork.BankAccountRepository.GetByID(userid);
+                      account.AccountBalance += amount;
+                      accountUnitOfWork.TransferRepository.Add(new Transfer()
+                      {
+                          Amount = amount,
+                          BankAccountId = account.Id,
+                          Username = account.UserName
+                      });
+                      accountUnitOfWork.BankAccountRepository.Update(account);
+                  }
+                  done = true;
+              }
+              catch (DbUpdateConcurrencyException ex)
+              {
+                  done = false;
+                  Thread.Sleep(1);
+              }
+          }
+          if(done)
+                return new BankRequestResult();
+          else
+                return new BankRequestResult(BankRequestStatus.Error, null, "Request timeout");
     }
 
     public BankRequestResult Withdraw(string userid, decimal amount)
     {
-      using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
-      {
-        try
+        bool done = false;
+        int iterations = 1;
+        while (iterations < AttemptsCount && !done)
         {
-          BankAccount account = accountUnitOfWork.BankAccountRepository.GetByID(userid);
+            iterations++;
+            try
+            {
+                using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+                {
+                    BankAccount account = accountUnitOfWork.BankAccountRepository.GetByID(userid);
 
-          if (account.AccountBalance < amount)
-          {
-            return new BankRequestResult(BankRequestStatus.Error, null, "Not enough money");
-          }
+                    if (account.AccountBalance < amount)
+                    {
+                        done = false;
+                        return new BankRequestResult(BankRequestStatus.Error, null, "Not enough money");
+                    }
 
-          account.AccountBalance -= amount;
+                    account.AccountBalance -= amount;
 
-          accountUnitOfWork.TransferRepository.Add(new Transfer()
-          {
-            Amount = -amount,
-            BankAccountId = account.Id,
-            Username = account.UserName
-          });
+                    accountUnitOfWork.TransferRepository.Add(new Transfer()
+                    {
+                        Amount = -amount,
+                        BankAccountId = account.Id,
+                        Username = account.UserName
+                    });
 
-          accountUnitOfWork.BankAccountRepository.Update(account);
+                    accountUnitOfWork.BankAccountRepository.Update(account);
+                    done = true;
+                }
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                done = false;
+                Thread.Sleep(1);
+            }
         }
-        catch (DbUpdateConcurrencyException ex)
-        {
-          return new BankRequestResult(BankRequestStatus.Error, null, "Concurency error");
-        }
-      }
-      return new BankRequestResult();
+        if (done)
+                return new BankRequestResult();
+        else
+                return new BankRequestResult(BankRequestStatus.Error, null, "Request timeout");
     }
 
-    public BankRequestResult Transfer(string sourceUserId, string destinationUserId, decimal amount)
-    {
-      using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+      public BankRequestResult Transfer(string sourceUserId, string destinationUserId, decimal amount)
       {
-        try
+        bool done = false;
+        int iterations = 1;
+
+        while (iterations < AttemptsCount && !done)
         {
-          BankAccount destAccount = accountUnitOfWork.BankAccountRepository.GetByID(destinationUserId);
-          BankAccount srcAccount = accountUnitOfWork.BankAccountRepository.GetByID(sourceUserId);
-          if (srcAccount.AccountBalance < amount)
-          {
-              return new BankRequestResult(BankRequestStatus.Error, null, "Not enouth money");
-          }
-          srcAccount.AccountBalance -= amount;
-          destAccount.AccountBalance += amount;
-          accountUnitOfWork.TransferRepository.Add(new Transfer()
-          {
-            Amount = amount,
-            BankAccountId = destAccount.Id,
-            Username = destAccount.UserName
-          });
-          accountUnitOfWork.TransferRepository.Add(new Transfer()
-          {
-            Amount = -amount,
-            BankAccountId = srcAccount.Id,
-            Username = srcAccount.UserName
-          });
-          accountUnitOfWork.BankAccountRepository.Update(srcAccount);
-          accountUnitOfWork.BankAccountRepository.Update(destAccount);
+            using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+            {
+                try
+                {
+                    BankAccount destAccount = accountUnitOfWork.BankAccountRepository.GetByID(destinationUserId);
+                    BankAccount srcAccount = accountUnitOfWork.BankAccountRepository.GetByID(sourceUserId);
+
+                    if (srcAccount.AccountBalance < amount)
+                    {
+                        done = false;
+                        return new BankRequestResult(BankRequestStatus.Error, null, "Not enouth money");
+                    }
+                    srcAccount.AccountBalance -= amount;
+                    destAccount.AccountBalance += amount;
+
+                    accountUnitOfWork.TransferRepository.Add(new Transfer()
+                    {
+                        Amount = amount,
+                        BankAccountId = destAccount.Id,
+                        Username = destAccount.UserName
+                    });
+
+                    accountUnitOfWork.TransferRepository.Add(new Transfer()
+                    {
+                        Amount = -amount,
+                        BankAccountId = srcAccount.Id,
+                        Username = srcAccount.UserName
+                    });
+
+                    accountUnitOfWork.BankAccountRepository.Update(srcAccount);
+                    accountUnitOfWork.BankAccountRepository.Update(destAccount);
+
+                    done = true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    done = false;
+                    Thread.Sleep(1);
+                }
+            }
         }
-        catch (DbUpdateConcurrencyException ex)
-        {
-          return new BankRequestResult(BankRequestStatus.Error, null, "Concurency error");
-        }
-      }
-      return new BankRequestResult();
+
+        if (done)
+            return new BankRequestResult();
+        else
+            return new BankRequestResult(BankRequestStatus.Error, null, "Request timeout");
     }
 
     public BankRequestResult CreateAccount(BankAccount account)
     {
-      using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
-        accountUnitOfWork.BankAccountRepository.Add(account);
-      return new BankRequestResult();
+        using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+        {
+            accountUnitOfWork.BankAccountRepository.Add(account);
+        }
+        return new BankRequestResult();
     }
 
     public BankRequestResult DeleteAccount(string id)
     {
-      using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
-        accountUnitOfWork.BankAccountRepository.Delete(accountUnitOfWork.BankAccountRepository.GetByID(id));
-      return new BankRequestResult();
+        using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+        {
+            accountUnitOfWork.BankAccountRepository.Delete(accountUnitOfWork.BankAccountRepository.GetByID(id));
+        }
+        return new BankRequestResult();
     }
 
     public BankRequestResult<IEnumerable<Transfer>> GetUserStatement(string accountid)
     {
         using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
-            return new BankRequestResult<IEnumerable<Transfer>>(BankRequestStatus.Done, accountUnitOfWork.TransferRepository.All(x=>x.BankAccountId==accountid));
+        {
+            return new BankRequestResult<IEnumerable<Transfer>>(BankRequestStatus.Done,
+                accountUnitOfWork.TransferRepository.All(x => x.BankAccountId == accountid));
+        }
     }
     
     public BankRequestResult<IQueryable<BankAccount>> AccountList()
     {
-      using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
-        return new BankRequestResult<IQueryable<BankAccount>>(BankRequestStatus.Done, accountUnitOfWork.BankAccountRepository.All());
+        using (AccountUnitOfWork accountUnitOfWork = this._factory.CreateAccountUnitOfWork())
+        {
+            return new BankRequestResult<IQueryable<BankAccount>>(BankRequestStatus.Done, accountUnitOfWork.BankAccountRepository.All());
+        }
     }
   }
 }
